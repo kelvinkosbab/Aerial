611b2a612995ea375060772a9eac4e597f27b9f7
Kelvin Kosbab <kelvin@kosbab.com>
2018-07-18 00:13:32 -0600

    Spacing updates.

--- Aerial/App/AppDelegate.swift
+++ Aerial/App/AppDelegate.swift
@@ -10,41 +10,41 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-    lazy var preferencesWindowController: PreferencesWindowController = PreferencesWindowController()
+  
+  @IBOutlet weak var window: NSWindow!
+  lazy var preferencesWindowController: PreferencesWindowController = PreferencesWindowController()
+  
+  func applicationDidFinishLaunching(_ notification: Notification) {
+    let objects = objectsFromNib(loadNibNamed: "PreferencesWindow")
     
-    func applicationDidFinishLaunching(_ notification: Notification) {
-        let objects = objectsFromNib(loadNibNamed: "PreferencesWindow")
-        
-        guard let windowIndex = objects.index(where: { $0 is NSWindow }),
-        let preferencesWindow = objects[windowIndex] as? NSWindow
-        else {
-            fatalError("Missing window object")
-        }
-        
-        setUp(preferencesWindow: preferencesWindow)
+    guard let windowIndex = objects.index(where: { $0 is NSWindow }),
+      let preferencesWindow = objects[windowIndex] as? NSWindow
+      else {
+        fatalError("Missing window object")
     }
     
-    private func setUp(preferencesWindow window: NSWindow) {
-        window.makeKeyAndOrderFront(self)
-        window.styleMask = [
-            NSWindow.StyleMask.titled,
-            NSWindow.StyleMask.closable,
-            NSWindow.StyleMask.miniaturizable
-        ]
-        
-        var frame = window.frame
-        frame.origin = window.frame.origin
-        window.setFrame(frame, display: true)
-    }
+    setUp(preferencesWindow: preferencesWindow)
+  }
+  
+  private func setUp(preferencesWindow window: NSWindow) {
+    window.makeKeyAndOrderFront(self)
+    window.styleMask = [
+      NSWindow.StyleMask.titled,
+      NSWindow.StyleMask.closable,
+      NSWindow.StyleMask.miniaturizable
+    ]
     
-    private func objectsFromNib(loadNibNamed nibName: String) -> [AnyObject] {
-        let bundle = Bundle.main
-		var topLevelObjects:NSArray? = NSArray()
-        bundle.loadNibNamed(NSNib.Name(nibName),
-                            owner: preferencesWindowController,
-                            topLevelObjects: &topLevelObjects)
-        return topLevelObjects! as [AnyObject]
-    }
+    var frame = window.frame
+    frame.origin = window.frame.origin
+    window.setFrame(frame, display: true)
+  }
+  
+  private func objectsFromNib(loadNibNamed nibName: String) -> [AnyObject] {
+    let bundle = Bundle.main
+    var topLevelObjects:NSArray? = NSArray()
+    bundle.loadNibNamed(NSNib.Name(nibName),
+                        owner: preferencesWindowController,
+                        topLevelObjects: &topLevelObjects)
+    return topLevelObjects! as [AnyObject]
+  }
 }
